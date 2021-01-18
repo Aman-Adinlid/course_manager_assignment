fcb8b1af82fea99b6a40292bf76f7783e6821d76
aman adinlid <95pinkpanda@gmail.com>
2021-01-19 00:55:40 +0100

    setter and getter

--- course_manager_assignment/src/main/java/se/lexicon/course_manager_assignment/model/Course.java
+++ course_manager_assignment/src/main/java/se/lexicon/course_manager_assignment/model/Course.java
@@ -1,5 +1,82 @@
 package se.lexicon.course_manager_assignment.model;
 
+import java.time.LocalDate;
+import java.util.Collection;
+
 public class Course {
 
+    private int Id;
+    private String course;
+    private LocalDate startDate;
+    private int weekDuration;
+    private Collection <Student> students;
+
+    public Course(int id, String course, LocalDate startDate, int weekDuration, Collection<Student> students) {
+        Id = id;
+        this.course = course;
+        this.startDate = startDate;
+        this.weekDuration = weekDuration;
+        this.students = students;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getWeekDuration() {
+        return weekDuration;
+    }
+
+    public void setWeekDuration(int weekDuration) {
+        this.weekDuration = weekDuration;
+    }
+
+    public Collection<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Collection<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "Id=" + Id +
+                ", course='" + course + '\'' +
+                ", startDate=" + startDate +
+                ", weekDuration=" + weekDuration +
+                ", students=" + students +
+                '}';
+    }
 }
